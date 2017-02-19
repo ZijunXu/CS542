7b17fde59b440afc0615d6746809f8bd135f87cd
YouthBread <YouthBread@users.noreply.github.com>
2017-02-18 21:35:00 -0500

    reg page set up, database not done yet

--- Flask_Server/app/forms.py
+++ Flask_Server/app/forms.py
@@ -1,8 +1,19 @@
-from flask_wtf import Form
-from wtforms import StringField, BooleanField, PasswordField
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField, PasswordField, validators
 from wtforms.validators import DataRequired
 
-class LoginForm(Form):
-    userId = StringField('UserID', validators=[DataRequired()])
-    userPW = PasswordField('Password', validators=[DataRequired()])
+class LoginForm(FlaskForm):
+    username = StringField('UserID', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
--- Flask_Server/app/views.py
+++ Flask_Server/app/views.py
@@ -1,9 +1,27 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, request
 from app import app
-from .forms import LoginForm
+from .forms import LoginForm,RegistrationForm
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     form = LoginForm()
+
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.UserID.data, str(form.remember_me.data)))
+        return redirect('/index')
+
     return render_template('index.html',title='Home',form=form,content='Hello, world!')
+
+
+@app.route('/reg', methods=['GET', 'POST'])
+def reg():
+    form = RegistrationForm()
+    if request.method == 'POST' and form.validate():
+        user = User(form.username.data, form.email.data,
+                    form.password.data)
+        db_session.add(user)
+        flash('Thanks for registering')
+        return redirect(url_for('login'))
+    return render_template('registration.html',title='Registration',form=form)
