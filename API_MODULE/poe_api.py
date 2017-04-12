37f55799fa2e9c8efffdc725a89e90ff8429c944
YouthBread <YouthBread@users.noreply.github.com>
2017-04-11 21:26:49 -0400

    modified the retrival json data

--- API_MODULE/poe_api.py
+++ API_MODULE/poe_api.py
@@ -2,6 +2,7 @@ from pymongo import MongoClient
 import requests
 import json
 import os
+import time
 
 
 class get_data_api:
@@ -31,6 +32,13 @@ class get_data_api:
         api_url = "http://api.pathofexile.com/public-stash-tabs/?id="
         r = requests.get(api_url + self.user_id)
         data = r.json()
+        while 'next_change_id' not in data:
+            r = requests.get(api_url + self.user_id)
+            data = r.json()
+            print("==================")
+            print("Sleep for 1 minute")
+            print("==================")
+            time.sleep(60)
         file_name = data['next_change_id']
         print("Current id: ", self.user_id + " next id: ", file_name)
         temp = []
@@ -41,6 +49,25 @@ class get_data_api:
                 for item in account['items']:
                     if 'note' in item:
                         item['owner'] = account['accountName']
+                        # set the value into int
+                        if 'requirements' in item:
+                            for req in item['requirements']:
+                                req['values'] = int(req['values'][0][0])
+
+                        # set the value into float
+                        if 'properties' in item:
+                            for prop in item['properties']:
+                                if len(prop['values']) >= 1:
+                                    if "%" in prop['values'][0][0]:
+                                        prop['values'] = float(prop['values'][0][0][:-1])
+                                    elif "-" in prop['values'][0][0]:
+                                        atk = [float(n) for n in prop['values'][0][0].split('-')]
+                                        prop['values'] = sum(atk)/len(atk)
+                                    else:
+                                        try:
+                                            prop['values'] = float(prop['values'][0][0])
+                                        except:
+                                            prop['values'] = prop['values'][0][0]
                         temp.append(item)
 
         if len(temp) != 0:
@@ -48,6 +75,8 @@ class get_data_api:
             db = client.project_542
             posts = db.posts
             posts.insert_many(temp)
+            print('Now MongoDB has %8i documents'%posts.count())
+            print("==================")
 
         self.record_last_user_id(file_name)
         self.user_id = file_name
