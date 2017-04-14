4a95bf865c0275a80080c417fd3851711b258c6d
you zhou <zhouyou66666@gmail.com>
2017-04-14 12:07:14 -0400

    token

--- Flask_Server/api_server/static/app-services/authentication.service.js
+++ Flask_Server/api_server/static/app-services/authentication.service.js
@@ -16,22 +16,7 @@
         return service;
 
         function Login(username, password, callback) {
-
-            /* Dummy authentication for testing, uses $timeout to simulate api call
-             ----------------------------------------------*/
-//            $timeout(function () {
-//                var response;
-//                UserService.GetByUsername(username)
-//                    .then(function (user) {
-//                        if (user !== null && user.password === password) {
-//                            response = { success: true };
-//                        } else {
-//                            response = { success: false, message: 'Username or password is incorrect' };
-//                        }
-//                        callback(response);
-//                    });
-//            }, 1000);
-
+            
             /* Use this for real authentication
              ----------------------------------------------*/
             $http.post('/api/authenticate', { username: username, password: password })
